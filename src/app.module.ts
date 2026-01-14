47748b132eb284238748fe1af778e5f61ff26c9c
Your Name <you@example.com>
2026-01-14 16:24:36 +0900

    cicd에도커에컨테이너까지곁들임;

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3307'),
+      username: process.env.DB_USER || 'son',
+      password: process.env.DB_PASSWORD || '1234',
+      database: process.env.DB_NAME || 'dockerTest',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      database: ':memory',
       synchronize: true,
       autoLoadEntities: true,
     }),
